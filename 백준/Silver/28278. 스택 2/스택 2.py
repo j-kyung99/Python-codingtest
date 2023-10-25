cf6b3557fe25ae1a6a555bae097057c203a864a2
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2023-10-25 14:54:31 +0900

    [Silver IV] Title: 스택 2, Time: 1372 ms, Memory: 71112 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+stack = []
+
+for _ in range(n):
+    order = list(map(int, sys.stdin.readline().split()))
+    if order[0] == 1:
+        stack.append(order[1])
+    elif order[0] == 2:
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+            stack.pop()
+    elif order[0] == 3:
+        print(len(stack))
+    elif order[0] == 4:
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    else:
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
